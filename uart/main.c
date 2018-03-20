3aeea346a876058f2665a4fdd6e42aec1e449e45
sigtot <sigurdtotland@gmail.com>
2018-03-20 15:08:05 +0100

    4.4

--- uart/main.c
+++ uart/main.c
@@ -4,8 +4,22 @@
 int main(){
     uart_init();
 
+    // Configure buttons
+    GPIO->PIN_CNF[17] = 0; // Button A
+    GPIO->PIN_CNF[26] = 0; // Button B
+
     while(1){
         uart_send(uart_read());
+
+        // Check if button A is pressed;
+        if(!(GPIO->IN & (1 << 17))) {
+            uart_send('A');
+        }
+
+        // Check if button B is pressed;
+        if(!(GPIO->IN & (1 << 26))) {
+            uart_send('B');
+        }
     }
 
     return 0;
